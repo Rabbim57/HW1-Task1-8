1e8e3908ca687a75e360b3a0c7626428eb23ebb3
Rabbim57 <Rabbim1998@mail.ru>
2023-08-19 15:25:49 +0300

    HW1-Task1-8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println("Количсетво собак = " + dog);
+        System.out.println("Количество кошек = " + cat);
+        System.out.println("Количсетво Бумаг = " + paper);
+
+        var dogPlus = (dog + 4);
+        var catPlus = (cat + 4);
+        var paperPlus = (paper + 4);
+
+        System.out.println("Количсетво собак после прибовления 4, равняется = " + dogPlus);
+        System.out.println("Количество кошек после прибовления 4, равняется = " + catPlus);
+        System.out.println("Количсетво Бумаг после прибовления 4, равняется = " + paperPlus);
+
+        var dogMinus = (dogPlus - 3.5);
+        var catMinus = (catPlus - 1.6);
+        var paperMinus = (paperPlus - 7639);
+
+        System.out.println("Количсетво собак после убавления 3,5, равняется = " + dogMinus);
+        System.out.println("Количество кошек после убавления 1,6, равняется = " + catMinus);
+        System.out.println("Количсетво Бумаг после убавления 7639, равняется = " + paperMinus);
+
+        var friend = 19;
+        var friendPlus = friend + 2;
+        var friendDivision = friendPlus / 7;
+        System.out.println("Количсетво друзей, равняется = " + friend);
+        System.out.println("Количсетво друзей, после прибавления 2, равняется = " + friendPlus);
+        System.out.println("Количсетво друзей, после деления на 7, равняется = " + friendDivision);
+
+        var frog = 3.5;
+        var frogIncrease = frog * 10;
+        var frogDivision = frogIncrease / 3.5;
+        var frogPlus = frogDivision + 4;
+
+        System.out.println("Количсетво лягушек, равняется = " + frog);
+        System.out.println("Количсетво ляшушек, после увелечения на 10, равняется = " + frogIncrease);
+        System.out.println("Количсетво ляшушек, после деления на 3,5, равняется = " + frogDivision);
+        System.out.println("Количсетво ляшушек, после прибавления, равняется = " + frogPlus);
+
+
+
+
+
+
+
+
+
     }
 }
